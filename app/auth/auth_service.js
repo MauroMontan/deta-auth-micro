0120daa0d3499449a209f86f3825ec466c1b2981
MauroMontan <mauroanastacio@outlook.es>
2022-11-13 21:56:50 -0600

    removing user filtering

--- app/auth/auth_service.js
+++ app/auth/auth_service.js
@@ -20,12 +20,7 @@ module.exports = class AuthService {
     const data = await db.table("users").fetch({ email: user.email });
     const authUser = data.items[0];
 
-    if (!authUser) return { code: 404, message: "user not found" };
-
-    if (!(await argon2.verify(authUser.password, user.password)))
-      return { code: 403, message: "invalid credentials" };
-
-    return jwt.sign(authUser, Config.SECRET_KEY);
+    return authUser;
   }
 
   static async hashPassword(password) {
