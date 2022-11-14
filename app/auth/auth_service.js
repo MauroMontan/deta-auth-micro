9b7640d46a741d331c96c5d26917a5a391e135c4
MauroMontan <mauroanastacio@outlook.es>
2022-11-13 22:04:05 -0600

    mocking email

--- app/auth/auth_service.js
+++ app/auth/auth_service.js
@@ -25,7 +25,7 @@ module.exports = class AuthService {
     if (!(await argon2.verify(authUser.password, user.password)))
       return { code: 403, message: "invalid credentials" };
 
-    return jwt.sign({ email: authUser.email }, Config.SECRET_KEY);
+    return jwt.sign({ email: "some_email" }, Config.SECRET_KEY);
   }
 
   static async hashPassword(password) {
