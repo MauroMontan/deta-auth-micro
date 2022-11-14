51fc06a1ff64046d73411d2c8b1a7fcd123a46a5
MauroMontan <mauroanastacio@outlook.es>
2022-11-13 22:01:42 -0600

    fixing sign token

--- app/auth/auth_service.js
+++ app/auth/auth_service.js
@@ -20,7 +20,12 @@ module.exports = class AuthService {
     const data = await db.table("users").fetch({ email: user.email });
     const authUser = data.items[0];
 
-    return authUser;
+    if (!authUser) return { code: 404, message: "user not found" };
+
+    if (!(await argon2.verify(authUser.password, user.password)))
+      return { code: 403, message: "invalid credentials" };
+
+    return jwt.sign({ email: authUser.email }, Config.SECRET_KEY);
   }
 
   static async hashPassword(password) {
